796584635af34a92d20ab7a92ae3c2e6ba6a17e6
Jss2k <cliffsclifus@icloud.com>
2021-05-10 19:59:02 +0300

    add a quote conponent draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <main className={styles.main}>
 
-    </div>
+    </main>
   )
 }
 
