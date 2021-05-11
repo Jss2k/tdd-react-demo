2b279471c9ffe02e4ad9394c516c4e06dd39b9c2
Jss2k <cliffsclifus@icloud.com>
2021-05-11 08:20:51 +0300

    use mock

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,7 +1,7 @@
-import { useConverter } from './useConverter'
+import { useConverter as converterHook } from './useConverter'
 import styles from './Converter.module.css'
 
-function Converter() {
+function Converter({ useConverter = converterHook }) {
   const { usd, rub, updateRub, updateUsd} = useConverter(100, 42)
 
   return (
--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -2,36 +2,61 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Converter from './Converter'
 
+const mockUpdateRub = jest.fn()
+const mockUpdateUsd = jest.fn()
+
+// jest.mock('./useConverter', () => ({
+//   useConverter() {
+//     return {
+//       rub: 100,
+//       usd: 2.38,
+//       updateRub: mockUpdateRub,
+//       updateUsd: mockUpdateUsd,
+//     }
+//   }
+// }))
+
+function useConverterMock() {
+  return {
+    rub: 100,
+    usd: 2.38,
+    updateRub: mockUpdateRub,
+    updateUsd: mockUpdateUsd,
+  }
+}
+
 describe('when rendered', () => {
   it('rub input should have a value with a rub amount', () => {
-    render(<Converter />)
+    render(<Converter useConverter={useConverterMock} />)
     expect(screen.getByLabelText(/Сумма в рублях/)).toHaveValue(100)
   })
 
   it('usd input should have a value with a usd amount', () => {
-    render(<Converter />)
+    render(<Converter useConverter={useConverterMock} />)
     expect(screen.getByLabelText(/Сумма в долларах/)).toHaveValue(2.38)
   })
 })
 
 describe('when typed in a RUB input', () => {
   it('should update its value', () => {
-    render(<Converter />)
+    render(<Converter useConverter={useConverterMock} />)
     const input = screen.getByLabelText(/Сумма в рублях/)
 
     userEvent.clear(input)
     userEvent.type(input, '42')
-    expect(input).toHaveValue(42)
+    // expect(input).toHaveValue(42)
+    expect(mockUpdateRub).toHaveBeenCalledWith('42')
   })
 })
 
 describe('when typed in a USD input', () => {
   it('should update its value', () => {
-    render(<Converter />)
+    render(<Converter useConverter={useConverterMock} />)
     const input = screen.getByLabelText(/Сумма в долларах/)
 
     userEvent.clear(input)
     userEvent.type(input, '42')
-    expect(input).toHaveValue(42)
+    // expect(input).toHaveValue(42)
+    expect(mockUpdateUsd).toHaveBeenCalledWith('42')
   })
 })
